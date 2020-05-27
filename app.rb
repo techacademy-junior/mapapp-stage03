9eda56e7fc30cbb7bc6e908a114f7f2ccc6213f5
tomoaki-kimura <tkim19750404@gmail.com>
2020-05-27 09:49:49 +0900

    text_1st_stage

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+enable :sessions
+
+get '/' do
+  @places = place_data
+  index = params[:index] ? params[:index].to_i : 0
+  data = @places[index]
+  @title = data ? data[:title] : ''
+  @latitude = data ? data[:latitude].to_f : '0.0'
+  @longitude = data ? data[:longitude].to_f : '0.0'
+  @memo = data ? data[:memo] : ''
+  erb :index
+end
+
+post '/' do
+  #ここに投稿するコードを書きます。
+end
+
+post '/delete' do
+  clear_place_data
+  redirect '/'
+end
+
+private
+
+def place_data
+  session[:place_data] || session[:place_data] = []
+end
+
+def clear_place_data
+  session[:place_data] = []
+end
+
+
+
